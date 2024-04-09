29bb9b9a2a7526231ac7b72d678cddf5bb401cee
bobbyhadz <opmftw@gmail.com>
2024-04-09 15:25:12 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Assign print output to a variable in Python
+
+example = 'bobbyhadz.com'
+
+# 👇️ remove call to print() to assign to a variable
+my_str = str(example)
+print(my_str) # 👉️ bobbyhadz.com
